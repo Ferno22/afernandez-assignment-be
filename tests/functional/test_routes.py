3edad03cb9a675a62c0fedb703a42cabb962dab7
Ferno22 <tonyfer002@gmail.com>
2022-12-07 13:57:46 +0100

    beta

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -1,13 +1,58 @@
-from iebank_api import app
+from cook_api import app
 import pytest
 
-def test_get_accounts(testing_client):
+def test_hello_world(testing_client):
     """
     GIVEN a Flask application
-    WHEN the '/accounts' page is requested (GET)
+    WHEN the '/' page is requested (GET)
     THEN check the response is valid
     """
-    response = testing_client.get('/accounts')
+    response = testing_client.get('/')
+    assert response.status_code == 200
+
+def test_get_recipes(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/recipes')
+    assert response.status_code == 200
+
+def test_create_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes' page is posted to (POST)
+    THEN check the response is valid
+    """
+    response = testing_client.post('/recipes', json={'name': 'recipe1', 'ingredients': 'ingredient1', 'steps': 'step1', 'rating': 1, 'favorite': False})
+    assert response.status_code == 200
+
+def test_get_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes/<name>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/recipes/recipe1')
+    assert response.status_code == 200
+
+def test_update_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes/<name>' page is posted to (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.put('/recipes/recipe1', json={'name': 'recipe1', 'ingredients': 'ingredient1', 'steps': 'step1', 'rating': 1, 'favorite': False})
+    assert response.status_code == 200
+
+def test_delete_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes/<name>' page is posted to (DELETE)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/recipes/recipe1')
     assert response.status_code == 200
 
 def test_dummy_wrong_path():
@@ -20,13 +65,23 @@ def test_dummy_wrong_path():
         response = client.get('/wrong_path')
         assert response.status_code == 404
 
-def test_create_account(testing_client):
+def test_dummy_wrong_method():
     """
     GIVEN a Flask application
-    WHEN the '/accounts' page is posted to (POST)
+    WHEN the '/recipes' page is posted to (GET)
     THEN check the response is valid
     """
-    response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
+    with app.test_client() as client:
+        response = client.get('/recipes', json={'name': 'recipe1', 'ingredients': 'ingredient1', 'steps': 'step1', 'rating': 1, 'favorite': False})
+        assert response.status_code == 405
+
+def test_delete_recipe(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/recipes/<name>' page is posted to (DELETE)
+    THEN check the response is valid
+    """
+    response = testing_client.delete('/recipes/recipe1')
     assert response.status_code == 200
 
 
