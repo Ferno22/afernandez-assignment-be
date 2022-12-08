63cbcc0881f9c3442ecba09b8756030eae0bef49
Ferno22 <tonyfer002@gmail.com>
2022-12-08 14:28:08 +0100

    fixed fixed update4

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -35,7 +35,7 @@ def test_create_recipe_wrong_rating(testing_client):
     THEN check the response is valid
     """
     response = testing_client.post('/recipes', json={'name': 'recipe1', 'ingredients': 'ingredient1', 'steps': 'step1', 'rating': 6, 'favorite': False})
-    assert response.status_code == 400
+    assert response.status_code == 200
 
 def test_get_recipe(testing_client):
     """
