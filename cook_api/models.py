c6e895a3bcbe24134cfbcae271a8f4617fa0ad11
Ferno22 <tonyfer002@gmail.com>
2022-12-09 01:08:01 +0100

    of course it did not work

--- cook_api/models.py
+++ cook_api/models.py
@@ -1,6 +1,6 @@
 from cook_api import db
 
-class Recipe(db.Module):
+class Recipe(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(32), nullable=False)
     ingredients = db.Column(db.String(100), nullable=False)
